0df26204c7760178fcac31e9cb7fcf074275f2c2
Kimirei <67070303@kmitl.ac.th>
2025-01-10 23:37:54 +0700

    Update Football.java

--- Football.java
+++ Football.java
@@ -1,27 +1,26 @@
-    class PlayerDetails {
-        private String team;
-        private String position;
-    
-        public void setName(String name) {
-        }
-    
-        public void setTeam(String team) {
-            this.team = team;
-        }
-    
-        public void setPlayerNumber(int playerNumber) {
-        }
-    
-        public void setPosition(String position) {
-            this.position = position;
-        }
-    
-        public boolean isSamePosition(PlayerDetails other) {
-            return this.position.equals(other.position);
-        }
-    
-        public boolean isSameTeam(PlayerDetails other) {
-            return this.team.equals(other.team);
-        }
+class FootballPlayer {
+    private String team;
+    private String position;
+
+    public void setName(String name) {
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public void setPlayerNumber(int playerNumber) {
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public boolean isSamePosition(FootballPlayer other) {
+        return this.position.equals(other.position);
+    }
+
+    public boolean isSameTeam(FootballPlayer other) {
+        return this.team.equals(other.team);
     }
 }
