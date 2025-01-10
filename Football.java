edc8a184693657bc60b8f8ceb8752c77f496640b
Kimirei <67070303@kmitl.ac.th>
2025-01-10 23:13:44 +0700

    Create Football.java

---
+++ Football.java
@@ -0,0 +1,28 @@
+public class FootballPlayer {
+    class PlayerDetails {
+        private String team;
+        private String position;
+    
+        public void setName(String name) {
+        }
+    
+        public void setTeam(String team) {
+            this.team = team;
+        }
+    
+        public void setPlayerNumber(int playerNumber) {
+        }
+    
+        public void setPosition(String position) {
+            this.position = position;
+        }
+    
+        public boolean isSamePosition(PlayerDetails other) {
+            return this.position.equals(other.position);
+        }
+    
+        public boolean isSameTeam(PlayerDetails other) {
+            return this.team.equals(other.team);
+        }
+    }
+}
