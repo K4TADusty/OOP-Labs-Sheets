b892a8c54b13958283a54e7581b0f0d159198c8e
Kimirei <67070303@kmitl.ac.th>
2025-01-10 23:29:26 +0700

    Create Private.java

---
+++ Private.java
@@ -0,0 +1,48 @@
+class Fraction {
+    int topN;
+    int btmN;
+
+    public Fraction(int top, int bottom) {
+        this.topN = top;
+        this.btmN = bottom;
+        simplify();
+    }
+
+    public Fraction() {
+        this.topN = 0;
+        this.btmN = 1;
+    }
+
+    public void addFraction(Fraction f) {
+        this.topN = this.topN * f.btmN + f.topN * this.btmN;
+        this.btmN = this.btmN * f.btmN;
+        simplify();
+    }
+
+    public String toFraction() {
+        return this.topN + "/" + this.btmN;
+    }
+
+    public double toFloat() {
+        return (double) this.topN / this.btmN;
+    }
+
+    public boolean equals(Fraction f) {
+        return this.topN == f.topN && this.btmN == f.btmN;
+    }
+
+    public void simplify() {
+        int gcd = gcd(this.topN, this.btmN);
+        this.topN /= gcd;
+        this.btmN /= gcd;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
